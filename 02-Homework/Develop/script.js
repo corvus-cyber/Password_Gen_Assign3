5efe44493dc790621fc1ddac95b4444f05a35d7d
Corvus <wyatta.hancock@gmail.com>
2020-06-03 10:03:14 -0600

    added prompts and confirms to ask the user what character types they want

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -3,12 +3,22 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var length = 8 - 128;
+    charAt= "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz012344556789!@#$%^&*()~+="
+
+  
+  var passwordLength = prompt("How many characters do you want for you password? Note: Must be between 8 to 128.") 
+  var passwordUpper = confirm("Do you want upper case letters in your password?");
+  var passwordLower = confirm("Do you want lower case letters in your password?");
+  var passwordSpecial = confirm("Do you want special characters in your password?");
+  var passwordNumber = confirm("Do you want numbers in your password?");  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  //
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
