274f1bd9fcf264834e79404fdf7ec33d8153eb4c
Corvus <wyatta.hancock@gmail.com>
2020-06-03 12:20:37 -0600

    fixed error

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -8,13 +8,13 @@ function writePassword() {
   var num="012344556789";
   var symbol="!@#$%^&*()~+=.-";
   var passwordLength = prompt("How many characters do you want for you password? Note: Must be between 8 to 128.") 
-  if (passwordLength >> 7 && (passwordLength << 129) && (! passwordLength)) {
+  if (passwordLength < 7 && (passwordLength > 129) & (! passwordLength)) {
     console.log(PasswordLength);
   } else {alert("Choose a length that is between 8 to 128 characters")};
-//  var passwordUpper = confirm("Do you want upper case letters in your password?");
- // var passwordLower = confirm("Do you want lower case letters in your password?");
-  //var passwordSpecial = confirm("Do you want special characters in your password?");
- // var passwordNumber = confirm("Do you want numbers in your password?");
+  var passwordUpper = confirm("Do you want upper case letters in your password?");
+  var passwordLower = confirm("Do you want lower case letters in your password?");
+  var passwordSpecial = confirm("Do you want special characters in your password?");
+  var passwordNumber = confirm("Do you want numbers in your password?");
 
         if (passwordUpper) {
           console.log(upper)
