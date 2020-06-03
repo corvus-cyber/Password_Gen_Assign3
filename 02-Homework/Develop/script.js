bafbd4b0a0cd1bbb07b9fa7b1ca0372daef14290
Corvus <wyatta.hancock@gmail.com>
2020-06-03 11:19:55 -0600

    added if/else

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -3,15 +3,24 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var length = 8 - 128;
-    charAt= "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz012344556789!@#$%^&*()~+="
-
-  
+  var lower= "abcdefghijklmnopqrstuvwxyz";
+  var upper= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var num="012344556789";
+  var symbol="!@#$%^&*()~+=.-";
   var passwordLength = prompt("How many characters do you want for you password? Note: Must be between 8 to 128.") 
   var passwordUpper = confirm("Do you want upper case letters in your password?");
   var passwordLower = confirm("Do you want lower case letters in your password?");
   var passwordSpecial = confirm("Do you want special characters in your password?");
-  var passwordNumber = confirm("Do you want numbers in your password?");  
+  var passwordNumber = confirm("Do you want numbers in your password?");
+  if (passwordLength) {
+    console.log(lower)
+  };
+  if (passwordUpper) {
+    console.log(upper)
+  };
+
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -21,4 +30,4 @@ function writePassword() {
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
