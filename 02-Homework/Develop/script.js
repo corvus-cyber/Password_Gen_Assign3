2ce87b5190d513606a1373bcb8b63a3e2adc9907
Corvus <wyatta.hancock@gmail.com>
2020-06-03 12:13:39 -0600

    pushed work so that assistants can see what is incorrect about the if/else statement

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -8,16 +8,26 @@ function writePassword() {
   var num="012344556789";
   var symbol="!@#$%^&*()~+=.-";
   var passwordLength = prompt("How many characters do you want for you password? Note: Must be between 8 to 128.") 
-  var passwordUpper = confirm("Do you want upper case letters in your password?");
-  var passwordLower = confirm("Do you want lower case letters in your password?");
-  var passwordSpecial = confirm("Do you want special characters in your password?");
-  var passwordNumber = confirm("Do you want numbers in your password?");
-  if (passwordLength) {
-    console.log(lower)
-  };
-  if (passwordUpper) {
-    console.log(upper)
-  };
+  if (passwordLength >> 7 && (passwordLength << 129) && (! passwordLength)) {
+    console.log(PasswordLength);
+  } else {alert("Choose a length that is between 8 to 128 characters")};
+//  var passwordUpper = confirm("Do you want upper case letters in your password?");
+ // var passwordLower = confirm("Do you want lower case letters in your password?");
+  //var passwordSpecial = confirm("Do you want special characters in your password?");
+ // var passwordNumber = confirm("Do you want numbers in your password?");
+
+        if (passwordUpper) {
+          console.log(upper)
+        };
+        if (passwordLower) {
+          console.log(lower)
+        };
+        if (passwordSpecial) {
+          console.log(symbol)
+        };
+        if (passwordNumber) {
+          console.log(num)
+        };
 
 
 
