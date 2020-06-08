32ba85cfc48749dfcf270e4921dfb99c8fe4687c
Corvus <wyatta.hancock@gmail.com>
2020-06-08 10:36:39 -0600

    added notes to all of my javascript so that an incoming web designer will know what everything will do. Also began filling out the ReadME file

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,9 +1,11 @@
+//Variables for the password criteria
 var Upper= "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
 var Lower= "abcdefghijklmnopqrstuvwxyz";
 var num= 1234567890;
 var special= "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-var reject= "please try again"
 
+//Variable for if the user decides not to choose acceptable criteria
+var reject= "please try again"
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -20,20 +22,23 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-
+//This function builds a randomized password based off of the User's choices 
 function createPassword() {
-  //Password length question
+  
+  //Variable for password length, kept as undefined so that if the user presses the button again it will be reset
   passwordLength = undefined;
+  //Empty variables that will be filled by the user's input
   var developPassword = "";
   var finalPassword = ""; 
+
+  //Password length question
   var passwordLength = prompt("How many characters do you want for you password? Note: Must be between 8 to 128.") 
   if (passwordLength > 7 && (passwordLength < 129)) {
     console.log(passwordLength) } 
     else {alert("Choose a length that is between 8 to 128 characters");
     return reject;
   }
-  //
+  //Question of whether the user wants to include uppercase letters in password
   uppercase = confirm("Do you want to include uppercase letters in your password?");
   if (uppercase) {
     developPassword += Upper;
@@ -41,7 +46,8 @@ function createPassword() {
    }
     else {alert("Uppercase letters will not be included in your password");
   }
-
+  
+  //Question of whether the user wants to include lowercase letters in password
   lowercase = confirm("Do you want to include lowercase letters in your password?");
   if (lowercase) {
     developPassword += Lower;
@@ -49,7 +55,8 @@ function createPassword() {
   }
     else {alert("Lowercase letters will not be included in your password"); 
   }
-    
+  
+  //Question of whether the user wants to include numbers in password
   numbers = confirm("Do you want to include numbers in your password?");
   if (numbers) {
     developPassword += num;
@@ -58,6 +65,7 @@ function createPassword() {
   else {alert("Numbers will not be included in your password");
   }
 
+  //Question of whether the user wants to include special characters in password
   characters = confirm("Do you want to include special characters in your password?");
   if (characters) {
     developPassword += special;
@@ -66,24 +74,23 @@ function createPassword() {
   else {alert("Special characters will not be included in your password");
   }
 
-
+  //This will build the password based off the criteria based off of the user's answers 
   for (var i = 1; i <=passwordLength; i++) {
     finalPassword += developPassword.charAt(Math.floor(Math.random() * developPassword.length));
     console.log(finalPassword);
   } 
-   
+  
+  //If the user decided not to include any of the four criteria, it will result in them being alerted that they need to choose at least one option
   if (finalPassword==="") {
     alert("You need to select one of the character types in order for the password to work");
     return reject;
   }
   
+  //If the user chose at at least one criteria, it will now populate the password in the box
   else {alert("Your password is ready!");
   return finalPassword; 
   }
-  
-
-
-  
+    
 }
 
 
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Password_Gen_Assign3
+
+# Purpose 
+The purpose of this assignment was to create a random password generator that would change based off of the chosen criteria that the user decided upon (the length of the password and whether to user, uppercase letters, lowercase letters, numbers and special characters). I built this project so as to be able to apply my new knowledge of javascript in a way that would be able to show off my newfound capabilities. This includes variables, functions, if/else statements, for loops, prompts, confirms, alerts, and console logs.    
+
+# Process 
+* Using a pencil and paper, I pseudocoded the process that would need to occur in order for this password to be randomly generated 
+* After making sure that the javascript was properly linked to the HTML I then created the necesscary variables for the password generator 
+* I then built a function that all of the javacript would be written in that will develop the password
+* I created a prompt that would ask the user the desired length of the password (between 8-128 characters). If they chose a length that did not meet the criteria, they would be alerted that they need to choose one within it and the function would end.
+* I then created a series of confirmation questions that would ask the user what kind of criteria they want for their password. I placed an if statement after each confirm that will populate an empty variable with the desired criteria. 
+* After all of the criteria had been chosen I built a for loop that would take the input of desired password length and criteria and would construct a randomized password. 
+* If the user chose at least one of the four criteria, their random password will populate in the box for the user to see. If not, it will alert them that they need to accept at least one of the four criteria. 
+
+# Issues I Encountered 
+* Originally I did not know that you could create an empty variable that would be populated at a later point. This led to a lot of confusion and frustration in figuring out how to compile the user's choices.
+* I did not know where to place the code originally, and placed my code within the pre-existing function writePassword instead of creating my own function.
+* I struggled to build a for loop that correctly chose the length 
+
+# What I Learned 
+
+# Link 
+
+# Screenshot 
+
